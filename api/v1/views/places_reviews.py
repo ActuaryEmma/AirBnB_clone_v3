7950bcddd2dafbaf2f786d7913609045ab12b4ef
ActuaryEmma <emmanyakio@gmail.com>
2023-11-01 18:27:00 +0300

    Task 11

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,16 +9,22 @@ from models.place import Place
 from models.review import Review
 from models.user import User
 
-@app_views.route("/places/<string:place_id>/reviews")
+
+@app_views.route("/places/<place_id>/reviews", methods=['GET'],
+                 strict_slashes=False)
 def get_all_reviews(place_id):
     """ get reviews from a spcific place """
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = [obj.to_dict() for obj in place.reviews]
+    reviews = []
+    for place in place.reviews:
+        reviews.append(place.to_dict())
     return jsonify(reviews)
 
-@app_views.route("/reviews/<string:review_id>")
+
+@app_views.route("/reviews/<review_id>", methods=['GET'],
+                 strict_slashes=False)
 def get_review(review_id):
     """ get review by id"""
     review = storage.get(Review, review_id)
@@ -26,7 +32,9 @@ def get_review(review_id):
         abort(404)
     return jsonify(review.to_dict())
 
-@app_views.route("/reviews/<string:review_id>")
+
+@app_views.route("/reviews/<review_id>", methods=['DELETE'],
+                 strict_slashes=False)
 def del_review(review_id):
     """ delete review by id"""
     review = storage.get(Review, review_id)
@@ -36,7 +44,9 @@ def del_review(review_id):
     storage.save()
     return jsonify({})
 
-@app_views.route("/reviews/<string:review_id>")
+
+@app_views.route("/places/<place_id>/reviews", methods=['POST'],
+                 strict_slashes=False)
 def create_obj_review(place_id):
     """ create new instance """
     place = storage.get(Place, place_id)
@@ -57,8 +67,9 @@ def create_obj_review(place_id):
     obj.save()
     return (jsonify(obj.to_dict()), 201)
 
-@app_views.route("/reviews/<string:review_id>")
-def post_review(review_id):
+
+@app_views.route("/reviews/<review_id>", strict_slashes=False, methods=['PUT'])
+def update_review(review_id):
     """ updates by id """
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
